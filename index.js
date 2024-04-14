e0a88d77069e57b90d87d70c0c19d687bbb20cc5
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:56:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// strarting with our car object 
+var car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+    color: "green",
+};
+// adding a new property color
+car.color = "red";
+// update a year
+car.year = 2021;
+console.log(car);
